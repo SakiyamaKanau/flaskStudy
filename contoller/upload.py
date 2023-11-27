7566809db8c74dfe53c8293bda7413eeb9a15199
SakiyamaKanau <kanaupc0708@gmail.com>
2023-11-27 17:31:26 +0000

    Initial commit

---
+++ contoller/upload.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, render_template, request
+from datetime import datetime
+
+upload_blueprint = Blueprint('upload', __name__)
+
+
+@upload_blueprint.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@upload_blueprint.route('/uploadFile', methods=['POST'])
+def uploadFile():
+    file = request.files['file']
+    if not file:
+        return 'ファイルがアップロードされていません', 400
+    file_content = file.stream.read().decode("utf-8")
+    process_csv_data(csv.reader(file_content.splitlines()))
+    return 'ファイルがアップロードされ、データが登録されました'
+
+def convert_date_format(japanese_date):
+  date_obj = datetime.strptime(japanese_date, '%Y年%m月%d日')
+  return date_obj.strftime('%Y-%m-%d')
+
+
+def process_csv_data(csv_reader):
+    """CSVデータを処理してデータベースに挿入"""
+    conn = connect_to_database()
+    cur = conn.cursor()
+    next(csv_reader)  # ヘッダ行をスキップ
+
+    for row in csv_reader:
+        #年の表記揺れを修正
+        row[3] = convert_date_format(row[3])  
+        cur.execute("INSERT INTO dummy_table (name, name_kana, age, birth_date, gender, blood_type, email, phone_number, mobile_phone_number, postal_code, address, company_name, credit_card_number, card_expiry, my_number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", row)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
---
+++ main.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+from controllers.upload import upload_blueprint
+import psycopg2
+
+
+
+app = Flask(__name__)
+# ブループリントの登録
+app.register_blueprint(upload_blueprint, url_prefix='/upload')
+
+
+
+def connect_to_database():
+    """PostgreSQLデータベースに接続"""
+    return psycopg2.connect(
+        dbname="neondb", 
+        user="neon", 
+        password="Ehgk5s2OvUKm", 
+        host="ep-late-forest-07793227.us-east-2.aws.neon.tech"
+    )
+
+#home
+@app.route('/')
+def index():
+    conn = connect_to_database()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM dummy_table")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', rows=rows)
+
+
+#run
+app.run(host='0.0.0.0', port=81)
+
